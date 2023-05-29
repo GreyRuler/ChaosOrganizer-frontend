3fab663c4e919476096f7694d23aac902e7f842b
GreyRuler <daniil_vasilenko757@mail.ru>
2023-05-29 13:58:48 +0300

    fix url

--- src/js/ChatView.js
+++ src/js/ChatView.js
@@ -111,7 +111,7 @@ export default class ChatView {
 		const chat = this.element.querySelector(
 			ChatView.selectorChat,
 		);
-		const ws = new WebSocket('ws://localhost:3030/ws');
+		const ws = new WebSocket('wss://chaosorganizer.onrender.com/ws');
 		const importBtn = this.element.querySelector(ChatView.selectorImportBtn);
 		const exportBtn = this.element.querySelector(ChatView.selectorExportBtn);
 		const searchBtn = this.element.querySelector(ChatView.selectorSearchBtn);
@@ -174,21 +174,21 @@ export default class ChatView {
 
 		importBtn.addEventListener('change', async () => {
 			const formData = this.prepareFiles([...importBtn.files]);
-			await fetch('http://localhost:3000/import', {
+			await fetch('https://chaosorganizer.onrender.com/import', {
 				method: 'POST',
 				body: formData,
 			});
 		});
 		exportBtn.addEventListener('click', async () => {
 			const filename = 'chaos-organizer-history.json';
-			const response = await fetch('http://localhost:3000/export');
+			const response = await fetch('https://chaosorganizer.onrender.com/export');
 			const json = await response.json();
 			this.downloadObjectAsJson(json, filename);
 		});
 		searchBtn.addEventListener('click', () => searchInput.classList.toggle('show'));
 		searchInput.addEventListener('input', async () => {
 			if (searchInput.value.trim()) {
-				const response = await fetch(`http://localhost:3000/search/${searchInput.value}`);
+				const response = await fetch(`https://chaosorganizer.onrender.com/search/${searchInput.value}`);
 				const json = await response.json();
 				const { messages } = json;
 				if (!messages.length) return;
@@ -214,7 +214,7 @@ export default class ChatView {
 				target.classList.toggle('active');
 			}
 			if (favouritesBtn.classList.contains('active')) {
-				const response = await fetch('http://localhost:3000/favourites');
+				const response = await fetch('https://chaosorganizer.onrender.com/favourites');
 				const json = await response.json();
 				const { messages } = json;
 				if (!messages.length) return;
@@ -240,7 +240,7 @@ export default class ChatView {
 		document.addEventListener('click', async (event) => {
 			const { target } = event;
 			if (target.classList.contains('favourites-message')) {
-				await fetch(`http://localhost:3000/favourites/${target.dataset.id}`, {
+				await fetch(`https://chaosorganizer.onrender.com/favourites/${target.dataset.id}`, {
 					method: 'POST',
 				});
 			}
@@ -276,7 +276,7 @@ export default class ChatView {
 	}
 
 	async loadMessages() {
-		const response = await fetch(`http://localhost:3000/messages/${this.count}`);
+		const response = await fetch(`https://chaosorganizer.onrender.com/messages/${this.count}`);
 		const { messages, noMoreData } = await response.json();
 		if (noMoreData) return;
 		this.count += 10;
@@ -296,7 +296,7 @@ export default class ChatView {
 	// eslint-disable-next-line consistent-return,class-methods-use-this
 	async sendFiles(formData) {
 		try {
-			const response = await fetch('http://localhost:3000/upload', {
+			const response = await fetch('https://chaosorganizer.onrender.com/upload', {
 				method: 'POST',
 				body: formData,
 			});
